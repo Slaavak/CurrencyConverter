f3f38090ee80fa74b53d0523e2b185bed733f17c
Slava Korolevich <slavak1998@gmail.com>
2025-05-29 23:40:33 +0200

    Main Screen | added history view for main screen

---
+++ CurrencyConverter/UI/HistoryView.swift
@@ -0,0 +1,16 @@
+//
+//  HistoryView.swift
+//  CurrencyConverter
+//
+//  Created by Slava Korolevich on 29/05/2025.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        List(0...100, id: \.self) { id in
+                Text("Number: \(id)")
+        }
+    }
+}
