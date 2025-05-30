49e8ab6dcb84b9b2bb4f46a2f195c59a283a21ab
Slava Korolevich <slavak1998@gmail.com>
2025-05-30 03:12:05 +0200

    Currency Service | fetchRates method is implemented

--- CurrencyConverter/Logic/CurrencyService.swift
+++ CurrencyConverter/Logic/CurrencyService.swift
@@ -6,3 +6,75 @@
 //
 
 import Foundation
+
+protocol CurrencyServiceProtocol {
+    func fetchRates(base: Currency, to currencies: [Currency]) async throws -> LatestRateResponse
+}
+
+enum CurrencyServiceError: Error {
+    case invalidURL
+    case invalidStatusCode
+    case invalidResponse
+    case decodingFailed
+}
+
+class CurrencyService {
+    private let urlBuilder: UrlBuilderProtocol
+    private let session: URLSessionProtocol
+
+    init(
+        urlBuilder: UrlBuilderProtocol = UrlBuilder(),
+        session: URLSessionProtocol = URLSession.shared
+    ) {
+        self.urlBuilder = urlBuilder
+        self.session = session
+    }
+}
+
+extension CurrencyService: CurrencyServiceProtocol {
+
+    func fetchRates(base: Currency, to currencies: [Currency]) async throws -> LatestRateResponse {
+        guard let url = urlBuilder.buildUrl(for: .latest(base: base, to: currencies)) else {
+            throw CurrencyServiceError.invalidURL
+        }
+
+        let response: LatestRateResponse = try await fetch(url: url)
+        return response
+    }
+}
+
+extension CurrencyService {
+    func fetch<T: Decodable>(url: URL) async throws -> T {
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+
+        self.log(request)
+        let (data, response): (Data, URLResponse)
+        do {
+            (data, response) = try await session.data(for: request)
+        } catch {
+            throw CurrencyServiceError.invalidResponse
+        }
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw CurrencyServiceError.invalidResponse
+        }
+
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw CurrencyServiceError.invalidStatusCode
+        }
+
+        do {
+            let result = try JSONDecoder().decode(T.self, from: data)
+            return result
+        } catch {
+            throw CurrencyServiceError.decodingFailed
+        }
+    }
+
+    func log(_ request: URLRequest) {
+        let log = "request url: \(request.url!.absoluteString)"
+        print(log)
+    }
+}
