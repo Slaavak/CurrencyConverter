24350374514d27f57e975cad188756b2b59ba691
Slava Korolevich <slavak1998@gmail.com>
2025-05-30 03:11:28 +0200

    URLSession | implement custom URLSession

--- CurrencyConverter/Logic/URLSession.swift
+++ CurrencyConverter/Logic/URLSession.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+protocol URLSessionProtocol {
+    func data(for request: URLRequest) async throws -> (Data, URLResponse)
+}
+
+extension URLSession: URLSessionProtocol {}
