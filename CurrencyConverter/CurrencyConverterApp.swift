96248cf6c863496c155e0457a23ef14aedb2145a
Slava Korolevich <slavak1998@gmail.com>
2025-06-01 18:40:07 +0200

    Record | change properties

--- CurrencyConverter/CurrencyConverterApp.swift
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -10,23 +10,9 @@ import SwiftData
 
 @main
 struct CurrencyConverterApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Record.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             HomeView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
