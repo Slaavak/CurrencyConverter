b42b824c6c8ab5a37383e45f38007704f51fb3ec
Slava Korolevich <slavak1998@gmail.com>
2025-06-02 10:56:31 +0200

    History View | Added history saving operation

--- CurrencyConverter/UI/Home/CurrencyConverter/CurrencyConverterViewModel.swift
+++ CurrencyConverter/UI/Home/CurrencyConverter/CurrencyConverterViewModel.swift
@@ -7,11 +7,13 @@
 
 import Combine
 import Foundation
+import SwiftUI
 
 @MainActor
-final class CurrencyConverterViewModel: ObservableObject {
+class CurrencyConverterViewModel: ObservableObject {
 
     var currencyService: CurrencyServiceProtocol
+    var dataSource: DataSource
 
     @Published var fromCurrency: Currency = .USD
     @Published var toCurrency: Currency = .EUR
@@ -22,11 +24,28 @@ final class CurrencyConverterViewModel: ObservableObject {
 
     private var cancellables = Set<AnyCancellable>()
 
-    init(currencyService: CurrencyServiceProtocol) {
+    init(currencyService: CurrencyServiceProtocol,
+              dataSource: DataSource) {
         self.currencyService = currencyService
+        self.dataSource = dataSource
 
         self.loadDefaultRates()
         self.setUpSubscriptions()
+    }
+}
+
+//MARK: - Public
+extension CurrencyConverterViewModel {
+    func saveConvertation() {
+        guard let fromAmount = Double(self.fromAmount),
+              let toAmount = Double(self.toAmount) else { return }
+
+        let entity = Record(timestamp: Date(),
+                         fromCurrency: fromCurrency,
+                            fromValue: fromAmount,
+                           toCurrency: toCurrency,
+                              toValue: toAmount)
+        dataSource.insert(entity)
     }
 }
 
@@ -34,26 +53,33 @@ final class CurrencyConverterViewModel: ObservableObject {
 private extension CurrencyConverterViewModel {
 
     func setUpSubscriptions() {
-        $fromCurrency.sink { [weak self] currency in
-            guard let self else { return }
-            UserDefaults.standard.set(currency.rawValue, forKey: "lastFromCurrency")
-            self.fetchRates(currency)
-        }
-        .store(in: &cancellables)
+        $fromCurrency
+            .sink { [weak self] currency in
+                guard let self else { return }
+                UserDefaults.standard.set(currency.rawValue, forKey: "lastFromCurrency")
+                self.fetchRates(currency)
+            }
+            .store(in: &cancellables)
 
-        $toCurrency.sink { [weak self] currency in
-            guard let self else { return }
-            UserDefaults.standard.set(currency.rawValue, forKey: "lastToCurrency")
-            rate = currentRates[currency]
-            updateToAmount()
-        }
-        .store(in: &cancellables)
+        $toCurrency
+            .sink { [weak self] currency in
+                guard let self else { return }
+                UserDefaults.standard.set(currency.rawValue, forKey: "lastToCurrency")
+                rate = currentRates[currency]
+                updateToAmount()
+            }
+            .store(in: &cancellables)
 
-        $fromAmount.sink { [weak self] _ in
-            guard let self else { return }
-            updateToAmount()
-        }
-        .store(in: &cancellables)
+        $fromAmount
+            .dropFirst(1)
+            .removeDuplicates()
+            .debounce(for: .seconds(2), scheduler: RunLoop.main)
+            .sink { [weak self] _ in
+                guard let self else { return }
+                self.updateToAmount()
+                self.saveConvertation()
+            }
+            .store(in: &cancellables)
     }
 
     func loadDefaultRates() {
